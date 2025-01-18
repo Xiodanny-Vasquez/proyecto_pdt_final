a8edd446dc4c9ef7221791ecae8b74fc7c6cea62
alejogto <stydduque@gmail.com>
2025-01-18 08:19:49 -0500

    agrege un pequeño footer para revisar funcionalidad del sistema

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-import Contact from "./Pages/Contact/Contact.jsx"
+import Contact from "./Pages/Contact/Contact.jsx";
+import Footer from "./Pages/Footer/Footer.jsx"; // Asegúrate de que el nombre esté en mayúscula
 
 function App() {
   return (
     <div>
-      <Contact/>
+      <Contact />
+      <Footer /> {/* Aquí se agrega el footer */}
     </div>
   );
 }
+
 export default App;
